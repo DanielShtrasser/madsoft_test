4b9c616a565bc5cc16dd1c4a14ddcd3fe0c02ba0
danielshtrasser <danielshtrasser@gmail.com>
2024-05-02 15:57:44 +0800

    more questions

--- src/App.tsx
+++ src/App.tsx
@@ -53,6 +53,125 @@ const data: Question[] = [
     ],
     correctAnswer: ["4"],
   },
+  {
+    type: "A",
+    id: 5,
+    questionTxt:
+      "Ipsum pariatur veniam qui ex quis irure ut commodo id ut excepteur tempor.",
+    answersVariants: [
+      {
+        text: "Et consequat Lorem irure anim dolor eiusmod culpa ea occaecat ut irure ut aliqua.",
+        id: "1",
+      },
+      {
+        text: "Elit exercitation do consequat aliquip adipisicing voluptate deserunt ipsum ullamco dolore in quis non sint.",
+        id: "2",
+      },
+      {
+        text: "Esse est eiusmod do et voluptate id duis proident in ex ex.",
+        id: "3",
+      },
+    ],
+    correctAnswer: ["4"],
+  },
+  {
+    type: "A",
+    id: 6,
+    questionTxt:
+      "Ipsum pariatur veniam qui ex quis irure ut commodo id ut excepteur tempor.",
+    answersVariants: [
+      {
+        text: "Duis mollit magna ipsum est commodo ut laboris nisi ut est excepteur duis est.",
+        id: "1",
+      },
+      { text: "Dolore excepteur elit et officia.", id: "2" },
+      { text: "Ullamco reprehenderit quis commodo id duis ea.", id: "3" },
+      { text: "Duis dolor Lorem nisi commodo aliquip duis.", id: "4" },
+    ],
+    correctAnswer: ["4"],
+  },
+  {
+    type: "A",
+    id: 7,
+    questionTxt:
+      "Ipsum pariatur veniam qui ex quis irure ut commodo id ut excepteur tempor.",
+    answersVariants: [
+      {
+        text: "Aute officia duis laboris ipsum ex dolor mollit ipsum.",
+        id: "1",
+      },
+      {
+        text: "In commodo voluptate est aute fugiat occaecat do irure sunt excepteur.",
+        id: "2",
+      },
+      { text: "Consectetur ullamco irure commodo commodo.", id: "3" },
+      {
+        text: "Duis nulla adipisicing sit magna sint irure deserunt occaecat deserunt est pariatur sint.",
+        id: "4",
+      },
+    ],
+    correctAnswer: ["4"],
+  },
+  {
+    type: "A",
+    id: 8,
+    questionTxt:
+      "Ipsum pariatur veniam qui ex quis irure ut commodo id ut excepteur tempor.",
+    answersVariants: [
+      { text: "In Lorem consectetur duis sit amet magna.", id: "1" },
+      {
+        text: "Laborum mollit et nulla magna laboris duis cupidatat aute commodo eiusmod aute.",
+        id: "2",
+      },
+      { text: "Ipsum consectetur do esse culpa minim.", id: "3" },
+      { text: "Fugiat enim et mollit sint.", id: "4" },
+    ],
+    correctAnswer: ["4"],
+  },
+  {
+    type: "A",
+    id: 9,
+    questionTxt:
+      "Ipsum pariatur veniam qui ex quis irure ut commodo id ut excepteur tempor.",
+    answersVariants: [
+      {
+        text: "Voluptate sunt et dolor consequat qui duis eiusmod velit consectetur dolore non est ut.",
+        id: "1",
+      },
+      {
+        text: "Est duis adipisicing enim sunt anim in proident fugiat ut cillum fugiat deserunt.",
+        id: "2",
+      },
+      {
+        text: "Eu ut et culpa irure cupidatat amet adipisicing occaecat aliqua esse.",
+        id: "3",
+      },
+    ],
+    correctAnswer: ["4"],
+  },
+  {
+    type: "A",
+    id: 10,
+    questionTxt:
+      "Ipsum pariatur veniam qui ex quis irure ut commodo id ut excepteur tempor.",
+    answersVariants: [
+      { text: "Culpa nulla irure cillum voluptate commodo nisi.", id: "1" },
+      {
+        text: "Occaecat adipisicing elit elit esse commodo qui culpa eu ex officia voluptate.",
+        id: "2",
+      },
+      { text: "Ipsum incididunt commodo excepteur fugiat.", id: "3" },
+      {
+        text: "Laborum incididunt velit ullamco amet sunt laboris qui proident pariatur magna.",
+        id: "4",
+      },
+      {
+        text: "Laborum irure elit exercitation ut anim tempor ullamco.",
+        id: "5",
+      },
+    ],
+    correctAnswer: ["4"],
+  },
 ];
 
 export default function App() {
